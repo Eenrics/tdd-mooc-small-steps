14af21879aa16549b5bea02a9761e9e3c4867397
eenrics <ebenezeresh525@gmail.com>
2024-05-05 22:43:24 +0300

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -31,7 +31,7 @@ function createApp(database: Database) {
     }
   }
 
-  
+  function parsePlainDate(dateString: string | undefined): undefined {}
 
   function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number) {
     if (type === "night") {
